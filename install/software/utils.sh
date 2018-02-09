6cbbf4e036c19c7bb396b260a4d89418c0347976
Michal Kimle <kimle.michal@gmail.com>
2018-02-09 13:05:18 +0100

    Add more common tools

--- install/software/utils.sh
+++ install/software/utils.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 function install_utils {
-  apt-get install -y vim nano git python python3
+  apt-get install -y vim nano git python python3 gettext-base screen tmux links lynx gawk rsync
 }
