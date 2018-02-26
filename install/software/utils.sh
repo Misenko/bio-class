e5077d41237c60649b7a397103706001dc143b6e
Michal Kimle <kimle.michal@gmail.com>
2018-02-26 08:23:19 +0100

    Add MC file manager

--- install/software/utils.sh
+++ install/software/utils.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 function install_utils {
-  apt-get install -y vim nano git python python3 gettext-base screen tmux links lynx gawk rsync
+  apt-get install -y vim nano git python python3 gettext-base screen tmux links lynx gawk rsync mc
 }
